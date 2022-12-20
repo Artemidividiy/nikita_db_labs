6c8bced2a6973de8154620504d35ded343cabfa2
Artemidividiy <artemiy.kasyanik@gmail.com>
2022-12-20 14:42:50 +0300

    table works

--- lab2/data_generator/generator.py
+++ lab2/data_generator/generator.py
@@ -1,4 +1,52 @@
+from faker import Faker
+from rich.console import Console
+from rich.table import Table
+
+
 class Generator:
     def __init__(self):
         pass
 
+
+class FakerWrapper:
+    def __init__(self):
+        self.faker = Faker()
+
+
+class Interface:
+    def __init__(self):
+        self.console = Console()
+
+    """ 
+        (a, b, c) = interface.ask_for_input("a","b","c")
+    """
+
+    def ask_for_input(self, *vars) -> list:
+        target = []
+        for i in vars:
+            self.console.log(f'provide {i=}')
+            target.append(input())
+        return target
+
+    """ to display table
+    vars: list(n, m+1) - table with **column names**
+    example:
+    ```python
+        interface.print_debug_table([["name", a, b], ["age", 12, 24]])
+    ```
+    | name | age |
+    | a    | 12  |
+    | b    | 24  |
+    """
+
+    def print_debug_table(self, vars: list):
+        self.console.log("print_debud_table method: [green] start [/green]")
+        target = Table()
+        for i in range(len(vars)):
+                target.add_column(f'{vars[i][0]}')
+            # что я высрал еб твою мать
+        for i in range(1, len(vars)): 
+            target.add_row(*[str(vars[_][i]) for _ in range(len(vars[i]))])
+        
+            
+        self.console.print(target)
--- lab2/data_generator/main.py
+++ lab2/data_generator/main.py
@@ -1,5 +1,12 @@
+from generator import Interface
+
+
 def main(): 
-    pass
+    interface = Interface()
+    
+    # a,b,c = interface.ask_for_input("a","b","c")
+    # interface.debug_output_obj()
+    interface.print_debug_table([["name", "a", "b"], ["age", 12, 14], ["tmp", 1, 2]])
 
 if __name__ == "__main__": 
     main()
